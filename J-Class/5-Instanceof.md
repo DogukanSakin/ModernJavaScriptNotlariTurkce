fa374ebb326ae99815cec3b2153c349243d1086d
DogukanSakin <dogukansakin1@hotmail.com>
2023-07-31 23:16:29 +0300

    instance of added.

---
+++ J-Class/5-Instanceof.md
@@ -0,0 +1,11 @@
+## Instanceof
+
+`instanceof` ile bir objenin bir class'ın instance'ı olup olmadığını kontrol edebiliriz.
+
+```js
+class Rabbit {}
+
+let rabbit = new Rabbit();
+
+alert( rabbit instanceof Rabbit ); // true
+```
--- test.js
+++ test.js
@@ -1,21 +1,3 @@
-class CoffeeMachine {
-    // ...
-  
-    constructor(power) {
-      this._power = power;
-    }
-  
-    get power() {
-      return this._power;
-    }
-  
-  }
-  
-  // create the coffee machine
-  let coffeeMachine = new CoffeeMachine(100);
-  
-  console.log(`Power is: ${coffeeMachine.power}W`); // Power is: 100W
-  
-  coffeeMachine.power = 25; // Error (no setter)
+const arr = [1,2,3];
 
-  console.log(`Power is: ${coffeeMachine.power}W`); // Power is: 100W
+console.log(Array.keys(arr));
